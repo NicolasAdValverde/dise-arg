c70bd1ac9a014940d4a8c143b2e2a1fbe9f30d10
Valverde Nicolas <valverdenicolasadrian@gmail.com>
2021-10-16 18:56:39 -0300

    se finalizo la version mobile

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 //EVENTOS
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+toggleButton.addEventListener("click", () => {
+    toggleButton.classList.toggle("close");
+    navWrapper.classList.toggle("show");
+});
+
+navWrapper.addEventListener("click", (e) => {
+    if (e.target.id === "nav") {
+        navWrapper.classList.remove("show");
+        toggleButton.classList.remove("close");
+    }
+});
 
 const boton = document.getElementById("boton");
 const texto = document.getElementById("text");
